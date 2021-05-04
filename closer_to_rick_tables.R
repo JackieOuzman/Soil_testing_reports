da53823b933365c716bec50907ffd40fa194c134
Jackie Ouzman <jackie.ouzman@csiro.au>
2021-05-04 15:44:51 +0930

    break it up into a few function - bit messy

--- closer_to_rick_tables.R
+++ closer_to_rick_tables.R
@@ -23,35 +23,39 @@ high_low_comp_t$comparison <- as.character(high_low_comp_t$comparison)
 str(high_low_comp_t$comparison)
 
 #comparison <-  "high_v_low"
-#comparison <-  "high_v_medium"
-comparison <-  "medium_v_low"
+comparison <-  "high_v_medium"
+#comparison <-  "medium_v_low"
 
 
 ##!!! User input required !!!!
 #Strip_type <-  "P Strip"
 Strip_type <-  "N Strip"
+
+
 ###############################################################################################
-# filter the data to one comparision - 
+# filter the data to one comparision -
 
 
+function_filter_data <- function(comparison,Strip_type, df ){
+  comparison <- quo_name(comparison)
+  high_v_low_comp_results <- df %>% 
+    dplyr::filter(comparison == !!comparison) 
 
-
-  
-  high_v_low_comp_results <- high_low_comp_t %>% 
-    filter(comparison == {{comparison}}) %>%  
-  dplyr::select('Zone ID' = Zone_ID,
+   
+ #################################################################### 
+  high_v_low_comp_results <- high_v_low_comp_results %>% 
+    dplyr::select('Zone ID' = Zone_ID,
                 Strip_Type,
                 
                 `yield with high fert` = high,
                 `yield with low fert` = low,
-                #medium,
+                `yield with medium fert` = medium,
                 
+                 high_vs_low,         
+                 high_vs_medium,
+                 medium_vs_low,
+                 
                 
-                `Mean yield difference` = high_vs_low,         
-                #high_vs_medium,
-                #medium_vs_low, 
-                
-                #`Mean yield difference` = rounded, 
                 "yield response" = yld_response,
                 `Significant` = Significant_practical) 
 high_v_low_comp_results <- high_v_low_comp_results %>% 
@@ -62,12 +66,12 @@ high_v_low_comp_results <- high_v_low_comp_results %>%
 high_v_low_comp_results <- high_v_low_comp_results %>% 
   mutate( `yield with high fert` = round(`yield with high fert`, digits = 2),
           `yield with low fert` = round(`yield with low fert`, digits = 2),
-          `Mean yield difference` = round(`Mean yield difference`, digits = 2))
+          `yield with medium fert` = round(`yield with medium fert`, digits = 2))
+          
 
 
 ##### This table has some key bits that we need... but we also need info from Sean DB
-
-
+###################################################################################################
 recom_rateDB <- read_excel( "W:/value_soil_testing_prj/Yield_data/2020/processing/GRDC 2020 Paddock Database_SA_VIC_April7 2021.xlsx")
 ##########################################################################################################################################
 
@@ -96,7 +100,6 @@ recom_rateDB <- recom_rateDB %>%
   )
 
 # join Sean results with the comparision
- 
 high_v_low_comp_results_Sean <- left_join(high_v_low_comp_results,recom_rateDB,
                           by = c(`Zone ID` = "Zone_ID" ))
 
@@ -116,32 +119,60 @@ high_v_low_comp_results_Sean <- high_v_low_comp_results_Sean %>%
     Strip_Type == 	"N Strip" & maxN <= 0 ~ "respose unlikely",
     TRUE ~ "NA"
   ))
+
+return(high_v_low_comp_results_Sean)}
+##########################################################################################################################
+#End of function call it and then user input required
+
+df_step1 <- function_filter_data(comparison, Strip_type, high_low_comp_t) 
+#assign(paste0(quo(comparison), "_", substr(Strip_type, start = 1, stop = 1)), missing stuff
+names(df_step1)
+
+##!!!! user input is required to select clms needed for analysis
+comparison
+df_step1 <- df_step1 %>%
+  dplyr::select( ## which one?
+                 #`Mean yield difference` = high_vs_low,
+                 `Mean yield difference` = high_vs_medium,
+                 #`Mean yield difference` = medium_vs_low,
+                 
+                 #plus all the others
+                 'Zone ID',
+                 Strip_Type,
+                 'yield with high fert',
+                 'yield with low fert',
+                 'yield with medium fert',
+                 'yield response',
+                 Significant,
+                 p_rec,
+                 SM_comment_Soil_N,
+                 SM_comment_Soil_P,
+                 SM_comment_Plant_Tissue,
+                 maxN,
+                 soil_test_indicates) %>% 
+  mutate(`Mean yield difference` = round(`Mean yield difference`, digits = 2))              
+
 ################################################################################################
-#### The data frame is now ready for summary stats.
+#### The data frame is now ready for summary stats. - df_step1
 ################################################################################################
 
-
+function_table <- function(comparison,Strip_type, df_step1 ){
 
 # count number of trials N and P for each yield resposne
 
-######################################################################################
-######################################################################################
-## filter set to strip type N or P
-
-
 ##########################################################################################
-x_trials_lowVsHigh_fert <- high_v_low_comp_results_Sean %>% filter(Strip_Type == Strip_type[1] ) %>% 
+x_trials_lowVsHigh_fert <- df_step1 %>% filter(Strip_Type == Strip_type[1] ) %>% 
   group_by(`yield response`) %>% 
   summarise("count by fert type" = n())
  
-x_soil_test_likley <-high_v_low_comp_results_Sean %>% 
+x_soil_test_likley <-df_step1 %>% 
   filter(Strip_Type == Strip_type[1] ) %>% 
   group_by(`yield response`, soil_test_indicates) %>% 
   summarise("count by soil test" = n()) %>% 
               arrange(soil_test_indicates)
 
-x_trials_lowVsHigh_fert  
-x_soil_test_likley 
+# x_trials_lowVsHigh_fert  
+# x_soil_test_likley 
 
 pivot1_count <- pivot_wider(x_trials_lowVsHigh_fert,
             names_from = `yield response`, 
@@ -155,11 +186,11 @@ pivot2_count <-pivot_wider(x_soil_test_likley,
   rename(grouping = soil_test_indicates)
 
 
-pivot1_count
-pivot2_count
+# pivot1_count
+# pivot2_count
 
 table_count <- rbind(pivot1_count, pivot2_count)
-table_count
+#table_count
 
 table_count$Sum <- rowSums(table_count[,1:3], na.rm=TRUE)
 table_count <- table_count %>%  mutate(
@@ -171,18 +202,22 @@ table_count <- table_count %>%  mutate(
 
 #######################################################################################################
 ### as above but with mean yield difference
-x_trials_lowVsHigh_fert_mean <- high_v_low_comp_results_Sean %>% filter(Strip_Type == Strip_type ) %>% 
-  group_by(`yield response`) %>% 
-  summarise("Average of yield difference" = mean(`Mean yield difference`,na.rm = TRUE )) 
 
-mean_soil_test_likley <-high_v_low_comp_results_Sean %>% 
-  filter(Strip_Type == Strip_type ) %>% 
+  x_trials_lowVsHigh_fert_mean <- df_step1 %>% filter(Strip_Type == Strip_type[1] ) %>%
+  group_by(`yield response`) %>% 
+  summarise('Average of yield difference' = mean(`Mean yield difference`,na.rm = TRUE )) 
+
+
+mean_soil_test_likley <-df_step1 %>% 
+  filter(Strip_Type == Strip_type[1] ) %>% 
   group_by(`yield response`, soil_test_indicates) %>% 
-  summarise("Average of yield difference" = mean(`Mean yield difference`,na.rm = TRUE )) %>% 
+  summarise('Average of yield difference' = mean(`Mean yield difference`,na.rm = TRUE )) %>% 
   arrange(soil_test_indicates)
 
-mean_soil_test_likley 
-x_trials_lowVsHigh_fert_mean
+
+
+# mean_soil_test_likley 
+# x_trials_lowVsHigh_fert_mean
 
 pivot1_mean <- pivot_wider(x_trials_lowVsHigh_fert_mean,
                             names_from = `yield response`, 
@@ -196,10 +231,10 @@ pivot2_mean <-pivot_wider(mean_soil_test_likley,
   rename(grouping = soil_test_indicates)
 
 
-pivot1_mean
-pivot2_mean
+# pivot1_mean
+# pivot2_mean
 table_mean <- rbind(pivot1_mean, pivot2_mean)
-table_mean
+#table_mean
 
 table_mean <- table_mean %>%  mutate(
   positive =      round(positive, digits = 3),
@@ -207,12 +242,13 @@ table_mean <- table_mean %>%  mutate(
   negative =     round(negative, digits = 3)
 )
 
+
 #########################################################################################
 table_mean <- table_mean %>%  dplyr::mutate(summary = "mean yld difference", Sum = "NA")
 table_count <- table_count %>%  dplyr::mutate(summary = "count")
 
-table_count
-table_mean
+# table_count
+# table_mean
 
 Table_1 <- data.frame(rbind(table_mean,table_count ))
 
@@ -244,12 +280,9 @@ display_table <- Table_1 %>%
     subtitle = "all zones processed - missing data has no recom rate (data filtered out")
 
 
- 
-name_table <- paste0(comparison, "_", substr(Strip_type, start = 1, stop = 1))
+return(display_table)}
 
-assign(name_table, display_table)
-
-name_table
+assign(paste0(comparison, "_", substr(Strip_type, start = 1, stop=1)),function_table(comparison,Strip_type, df_step1) )
 
 ## check the output ##
 
